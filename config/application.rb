7710e5e549705a74d9441dd67632751d286e816a
thanhl2t <lethanhtb96@gmail.com>
2018-11-05 08:35:32 +0700

    rails flavored ruby

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,17 @@ Bundler.require(*Rails.groups)
 
 module ToyApp
   class Application < Rails::Application
+  config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+  config.i18n.available_locales = [:en, :vi]
+  config.i18n.default_locale = :en
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
+
+  def set_locale
+    locale = params[:locale].to_s.strip.to_sym
+    I18n.locale = I18n.available_locales.include?(locale) ? locale : I18n.default_locale
+	end
 end
