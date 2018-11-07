fc58bdd3e2adc4f50b525bb3e0efc3a1decd223f
thanhl2t <lethanhtb96@gmail.com>
2018-11-07 08:11:01 +0700

    Request 1 part 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'session/new'
+  get "users/new"
+  get "/signup", to: "users#new"
+  root "static_pages#home"
+  get "/help", to: "static_pages#help"
+  get "/about", to: "static_pages#about"
+  get "/contact", to: "static_pages#contact"
+  get "/login", to: "session#new"
+  post "/signup", to: "session#create"
+  delete "/logout", to: "session#destroy"
+  resources :users
 end
